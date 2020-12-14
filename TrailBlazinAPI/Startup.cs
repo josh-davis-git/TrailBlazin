79594c2d7e9e5f82bb1b099690e80607f13e1e7a
Joshua Davis <53221972+josh-davis-git@users.noreply.github.com>
2020-12-14 15:36:13 -0500

    project complete

--- TrailBlazinAPI/Startup.cs
+++ TrailBlazinAPI/Startup.cs
@@ -1,18 +1,24 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using TrailBlazinAPI.Data;
 using TrailBlazinAPI.Repository;
@@ -33,44 +39,98 @@ namespace TrailBlazinAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
+            services.AddDbContext<ApplicationDbContext>
+                (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddScoped<INationalParkRepository, NationalParkRepository>();
             services.AddScoped<ITrailRepository, TrailRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(TrailMapping));
-            services.AddSwaggerGen(options =>
+            services.AddApiVersioning(options =>
             {
-                options.SwaggerDoc("TrailBlazinOpenAPISpec",
-                    new Microsoft.OpenApi.Models.OpenApiInfo()
-                    {
-                        Title = "Trail Blazin' API",
-                        Version = "1",
-                        Description = "Trail Blazin Project",
-                        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                        {
-                            Email = "josh.davis.work@outlook.com",
-                            Name = "Josh Davis",
-                            Url = new Uri("https://www.localhost:5001")
-                        },
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+            services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen();
+            var appSettingsSection = Configuration.GetSection("AppSettings");
 
-                        License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                        {
-                            Name = "MIT License",
-                            Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
-                        }
+            services.Configure<AppSettings>(appSettingsSection);
 
-                    }); 
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
 
-                var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
-                options.IncludeXmlComments(cmlCommentsFullPath);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x => {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
             });
 
+
+            //services.AddSwaggerGen(options=> {
+            //    options.SwaggerDoc("ParkyOpenAPISpec",
+            //        new Microsoft.OpenApi.Models.OpenApiInfo()
+            //        {
+            //            Title = "Parky API",
+            //            Version = "1",
+            //            Description="Udemy Parky API NP",
+            //            Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+            //            {
+            //                Email = "bhrugen.udemy@gmail.com",
+            //                Name = "Bhrugen Patel",
+            //                Url = new Uri("https://wwww.bhrugen.com")
+            //            },
+            //            License = new Microsoft.OpenApi.Models.OpenApiLicense()
+            //            {
+            //                Name = "MIT License",
+            //                Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+            //            }
+
+            //        });
+
+            //    //options.SwaggerDoc("ParkyOpenAPISpecTrails",
+            //    //   new Microsoft.OpenApi.Models.OpenApiInfo()
+            //    //   {
+            //    //       Title = "Parky API Trails",
+            //    //       Version = "1",
+            //    //       Description = "Udemy Parky API Trails",
+            //    //       Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+            //    //       {
+            //    //           Email = "bhrugen.udemy@gmail.com",
+            //    //           Name = "Bhrugen Patel",
+            //    //           Url = new Uri("https://wwww.bhrugen.com")
+            //    //       },
+            //    //       License = new Microsoft.OpenApi.Models.OpenApiLicense()
+            //    //       {
+            //    //           Name = "MIT License",
+            //    //           Url = new Uri("https://en.wikipedia.org/wiki/MIT_License")
+            //    //       }
+
+            //    //   });
+            //    var xmlCommentFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //    var cmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentFile);
+            //    options.IncludeXmlComments(cmlCommentsFullPath);
+            //});
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -78,16 +138,25 @@ namespace TrailBlazinAPI
             }
 
             app.UseHttpsRedirection();
-
             app.UseSwagger();
-            app.UseSwaggerUI(options => 
-            {
-                options.SwaggerEndpoint("/swagger/TrailBlazinOpenAPISpec/swagger.json", "Trail Blazin' API");
+            app.UseSwaggerUI(options => {
+                foreach (var desc in provider.ApiVersionDescriptions)
+                    options.SwaggerEndpoint($"/swagger/{desc.GroupName}/swagger.json",
+                        desc.GroupName.ToUpperInvariant());
                 options.RoutePrefix = "";
             });
 
+            //app.UseSwaggerUI(options=> {
+            //    options.SwaggerEndpoint("/swagger/ParkyOpenAPISpec/swagger.json", "Parky API");
+            //    //options.SwaggerEndpoint("/swagger/ParkyOpenAPISpecTrails/swagger.json", "Parky API Trails");
+            //    options.RoutePrefix = "";
+            //});
             app.UseRouting();
-
+            app.UseCors(x => x
+              .AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
--- TrailBlazinClient/SD.cs
+++ TrailBlazinClient/SD.cs
@@ -5,11 +5,12 @@ using System.Threading.Tasks;
 
 namespace TrailBlazinClient
 {
-    public class SD
+    public static class SD
     {
         public static string APIBaseUrl = "https://localhost:44346/";
-        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks";
-        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/v1/nationalparks/";
+        public static string TrailAPIPath = APIBaseUrl + "api/v1/trails/";
+        public static string AccountAPIPath = APIBaseUrl + "api/v1/Users/";
 
     }
 }
